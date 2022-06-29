94a0fd9c2930f4f7e41991a1c2d6c493237b687f
Krish <Bobbasaikrishna1995@gmail.com>
2022-06-29 16:30:20 +0530

    Initial application setup with all dependencies

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+
+const App = () => {
+  return (
+    <div className="App">
+      <div id="loader-wrapper">
+        <div id="loader"></div>
+        <div className="loader-section section-left"></div>
+        <div className="loader-section section-right"></div>
+      </div>
+    </div>
+  );
+};
+export default App;
