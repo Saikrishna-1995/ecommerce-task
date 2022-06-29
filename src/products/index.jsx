e12181e5e75c411c5f9ded9d0ecd0b4bf0c78bba
Krish <Bobbasaikrishna1995@gmail.com>
2022-06-29 16:46:53 +0530

    Integrated search filter and filterby type dropdown

--- src/products/index.jsx
+++ src/products/index.jsx
@@ -5,12 +5,27 @@ import Api from "../Api";
 
 const Products = () => {
     const [productsData, setProductsData] = useState([]);
+    const [productTypes, setProductTypes] = useState([]);
+    const [filteredData, setFilteredData] = useState([]);
 
     useEffect(() => {
         Api.get("assets/products.json")
             .then((response) => {
                 if (response.status === 200) {
                     setProductsData(response.data);
+                    setFilteredData(response.data);
+                    var setObj = new Set();
+                    var result = response.data.reduce((acc, item) => {
+                        if (!setObj.has(item.type)) {
+                            setObj.add(item.type, item)
+                            acc.push({
+                                value: item.type,
+                                label: item.type
+                            })
+                        }
+                        return acc;
+                    }, []);
+                    setProductTypes([{ value: "All", label: "All Products" }, ...result])
                     setTimeout(() => {
                         document.body.classList.add("loaded");
                     }, 2000);
@@ -21,6 +36,26 @@ const Products = () => {
             });
     }, []);
 
+    function SelectType(e, fieldType) {
+        if (fieldType === 'byInput') {
+            const test = productsData.filter(team => {
+                return team.productName.toLowerCase().includes(e.toLowerCase());
+            });
+            setFilteredData(test);
+        }
+        else {
+            if (e.value === 'All') {
+                setFilteredData(productsData);
+            } else {
+                const itemList = productsData.filter(item => {
+                    return item.type === e.value;
+                });
+                setFilteredData(itemList);
+            }
+        }
+
+    };
+
     return (
         <div className="container-fluid tm-container-content tm-mt-60">
             <div className="row">
@@ -28,17 +63,26 @@ const Products = () => {
                     <h2 className="tm-text-primary">Products</h2>
                 </div>
                 <div className="col-xl-6 col-lg-6 col-md-6 col-sm-6 col-12 mb-5 blocks">
-
+                    <InputGroup className="mb-3">
+                        <InputGroup.Text id="basic-addon1">Search</InputGroup.Text>
+                        <FormControl
+                            placeholder="Product Name"
+                            aria-label="name"
+                            aria-describedby="basic-addon1"
+                            onChange={(e) => SelectType(e.target.value, 'byInput')}
+                        />
+                    </InputGroup>
                 </div>
                 <div className="col-xl-3 col-lg-3 col-md-6 col-sm-6 col-12 mb-5 blocks">
 
-
+                    <Select options={productTypes}
+                        onChange={(e) => SelectType(e, 'byDropdown')} placeholder='Filter By type' />
 
                 </div>
             </div>
             <div className="row tm-mb-90 tm-gallery main-block">
                 {
-                    productsData.map((item, i) => {
+                    filteredData.map((item, i) => {
                         return (
                             <div
                                 className="col-xl-3 col-lg-3 col-md-6 col-sm-6 col-12 mb-5 blocks"
